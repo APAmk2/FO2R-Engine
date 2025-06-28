7239ee712780717e680883efc416b41c0e02d964
APAMk2 <APAmk2@users.noreply.github.com>
2025-06-28 18:09:29 +0400

    Some work with animations
    - Reworked animation forming
     - Deleted ANIM_FLAG_FIRST_FRAME and ANIM_FLAG_LAST_FRAME flags
    - In animation forming event arguments beginFrame & endFrame are passed, that sets from which to which frame animation will be played

--- Source/Mapper.cpp
+++ Source/Mapper.cpp
@@ -5349,9 +5349,9 @@ void FOMapper::InitScriptSystem()
         { &MapperFunctions.KeyDown, "key_down", "bool %s(uint8,string&)" },
         { &MapperFunctions.KeyUp, "key_up", "bool %s(uint8,string&)" },
         { &MapperFunctions.InputLost, "input_lost", "void %s()" },
-        { &MapperFunctions.CritterAnimation, "critter_animation", "string@ %s(int,uint,uint,uint,uint&,uint&,int&,int&)" },
-        { &MapperFunctions.CritterAnimationSubstitute, "critter_animation_substitute", "bool %s(int,uint,uint,uint,uint&,uint&,uint&)" },
-        { &MapperFunctions.CritterAnimationFallout, "critter_animation_fallout", "bool %s(uint,uint&,uint&,uint&,uint&,uint&)" },
+		{ &MapperFunctions.CritterAnimation, "critter_animation", "string@ %s(int,uint,uint,uint,uint&,int&,int&,int&,int&)" },
+		{ &MapperFunctions.CritterAnimationSubstitute, "critter_animation_substitute", "bool %s(int,uint,uint,uint,uint&,uint&,uint&)" },
+		{ &MapperFunctions.CritterAnimationFallout, "critter_animation_fallout", "bool %s(uint,uint&,uint&,uint&,uint&,int&,int&)" },
     };
     Script::BindReservedFunctions( (char*) scripts_cfg.GetBuf(), "mapper", BindGameFunc, sizeof( BindGameFunc ) / sizeof( BindGameFunc[ 0 ] ) );
 
